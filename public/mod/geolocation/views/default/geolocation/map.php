dae4e99124810dae1ac0d2a38ea1035e6ee4ef31
Ildar Manzhikov <ildar.manzhikov@flatsoft.com>
2010-04-29 16:47:33 +0400

    Added select region on map

--- public/mod/geolocation/views/default/geolocation/map.php
+++ public/mod/geolocation/views/default/geolocation/map.php
@@ -14,7 +14,7 @@
 	jQuery(function() {
 		map = new google.maps.Map2(document.getElementById("map"));
 		map.setUIToDefault();
-		refreshMap(data);				
+		refreshMap(data);
 	});
 
 	function toggleAll(toggle) {
@@ -45,6 +45,7 @@
 
 		var center = bounds.getCenter();
 		var zoom   = map.getBoundsZoomLevel(bounds);
+				
 		map.setCenter(center, zoom);
 
 		map.addControl(new GLargeMapControl());
@@ -67,7 +68,75 @@
             });
             return false; // <-- important!
         });
+
+		$('#geosearch').submit(function() {
+			geocode();
+			return false;
+		});
     });
+
+	function geocode() {
+		var query = document.getElementById("query").value;
+		if (/\s*^\-?\d+(\.\d+)?\s*\,\s*\-?\d+(\.\d+)?\s*$/.test(query)) {
+			var latlng = parseLatLng(query);
+			if (latlng == null) {
+				document.getElementById("query").value = "";
+			} else {
+				reverseGeocode(latlng);
+			}
+		} else {
+			forwardGeocode(query);
+		}
+	}
+
+	function initGeocoder(query) {
+		selected = null;
+
+		var hash = 'q=' + query;
+		var geocoder = new GClientGeocoder();
+
+		hashFragment = '#' + escape(hash);
+		window.location.hash = escape(hash);
+		return geocoder;
+	}
+
+	function forwardGeocode(address) {
+		var geocoder = initGeocoder(address);
+		geocoder.getLocations(address, function(response) {
+			showResponse(response, false);
+		});
+	}
+
+	function reverseGeocode(latlng) {
+		var geocoder = initGeocoder(latlng.toUrlValue(6));
+		geocoder.getLocations(latlng, function(response) {
+			showResponse(response, true);
+		});
+	}
+
+	function parseLatLng(value) {
+		value.replace('/\s//g');
+		var coords = value.split(',');
+		var lat = parseFloat(coords[0]);
+		var lng = parseFloat(coords[1]);
+		if (isNaN(lat) || isNaN(lng)) {
+			return null;
+		} else {
+			return new GLatLng(lat, lng);
+		}
+	}
+
+	function showResponse(response, reverse) {
+		if (! response) {
+			alert("Geocoder request failed");
+		} else {
+			latlng = new GLatLng(response.Placemark[0].Point.coordinates[1],
+			response.Placemark[0].Point.coordinates[0]);			
+			map.panTo(latlng);
+			//refreshMap(data, latlng)
+		}
+	}
+
 </script>
 
 
@@ -83,6 +152,12 @@
 			<li>Select: <a href="javascript:toggleAll(1)">All</a> | <a href="javascript:toggleAll(0)">None</a></li>
 			<li><input type="submit" name="do" value="Update map"></li>
 		</ul>
+	</form>
+</div>
+<div class="geosearch">
+	<form name="geosearch" id="geosearch" onsubmit="return false;">
+		<input type="text" name="query" id="query" value=""/>
+		<input type="submit" id="query_submit" value="Search" />
 	</form>
 </div>
 <div id="map" style="width: 100%">

83ac68da0cec548cff4d9854e9aae62446ade812
Ildar Manzhikov <ildar.manzhikov@flatsoft.com>
2010-05-05 18:53:28 +0400

    Fix in profile if locations not exists

--- public/mod/geolocation/views/default/geolocation/profile_userdetails.php
+++ public/mod/geolocation/views/default/geolocation/profile_userdetails.php
@@ -1,18 +1,26 @@
 <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?= $GLOBALS['google_api'] ?>" type="text/javascript"></script>
+<script type="text/javascript">
+	function show_map_and_marker(map_id, latlng) {
+		map = new google.maps.Map2(document.getElementById(map_id));
+		map.setCenter(latlng, 13);
+		map.addOverlay(new GMarker(latlng));
+	}
+</script>
+<?php if($vars['entity']->current_latitude && $vars['entity']->current_longitude): ?>
 <div class="map_current">
 	<h4>Current location</h4>
 	<div id="map_current"></div>
 </div>
+<script type="text/javascript">
+	show_map_and_marker('map_current', new GLatLng('<?php echo $vars['entity']->current_latitude; ?>', '<?php echo $vars['entity']->current_longitude; ?>'));
+</script>
+<?php endif; ?>
+<?php if($vars['entity']->home_latitude && $vars['entity']->home_longitude): ?>
 <div class="map_home">
 	<h4>Home location</h4>
 	<div id="map_home"></div>
 </div>
-<script>
-	function show_map_and_marker(map_id, latlng) {
-		map = new google.maps.Map2(document.getElementById(map_id));		
-		map.setCenter(latlng, 10);
-		map.addOverlay(new GMarker(latlng));
-	}	
+<script type="text/javascript">
 	show_map_and_marker('map_home', new GLatLng('<?php echo $vars['entity']->home_latitude; ?>', '<?php echo $vars['entity']->home_longitude; ?>'));
-	show_map_and_marker('map_current', new GLatLng('<?php echo $vars['entity']->current_latitude; ?>', '<?php echo $vars['entity']->current_longitude; ?>'));
 </script>
+<?php endif; ?>

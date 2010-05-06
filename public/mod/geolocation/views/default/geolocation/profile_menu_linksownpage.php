9c7929d6979eb61b444f22ed86fad8be929fab0c
Ildar Manzhikov <ildar.manzhikov@flatsoft.com>
2010-05-06 16:39:25 +0400

    Added JS to show curr/home locs

--- public/mod/geolocation/views/default/geolocation/profile_menu_linksownpage.php
+++ public/mod/geolocation/views/default/geolocation/profile_menu_linksownpage.php
@@ -1,4 +1,12 @@
 <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?= $GLOBALS['google_api'] ?>" type="text/javascript"></script>
+<p>
+	<a href="javascript:show_map_and_marker('map_current', new GLatLng('<?php echo $vars['entity']->current_latitude; ?>', '<?php echo $vars['entity']->current_longitude; ?>'));"><?php echo elgg_echo("geolocation:current_location"); ?></a>
+	<div id="map_current"></div>
+</p>
+<p>
+	<a href="javascript:show_map_and_marker('map_home', new GLatLng('<?php echo $vars['entity']->home_latitude; ?>', '<?php echo $vars['entity']->home_longitude; ?>'));"><?php echo elgg_echo("geolocation:home_location"); ?></a>
+	<div id="map_home"></div>
+</p>
 <script type="text/javascript">
 	function show_map_and_marker(map_id, latlng) {
 		map = new google.maps.Map2(document.getElementById(map_id));
@@ -6,9 +14,3 @@
 		map.addOverlay(new GMarker(latlng));
 	}
 </script>
-<p class="user_menu_friends">
-	<a href="<?php echo $vars['url']; ?>pg/friends/<?php echo $vars['entity']->username; ?>/"><?php echo elgg_echo("geolocation:current_location"); ?></a>
-</p>
-<p class="user_menu_friends_of">
-	<a href="<?php echo $vars['url']; ?>pg/friendsof/<?php echo $vars['entity']->username; ?>/"><?php echo elgg_echo("geolocation:home_location"); ?></a>
-</p>

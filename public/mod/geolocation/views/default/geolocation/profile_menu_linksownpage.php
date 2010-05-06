98d307d61959dda34e03936b58f7e810b2fc4ceb
Ildar Manzhikov <ildar.manzhikov@flatsoft.com>
2010-05-06 17:27:31 +0400

    Fix popup in home/cur locs

--- public/mod/geolocation/views/default/geolocation/profile_menu_linksownpage.php
+++ public/mod/geolocation/views/default/geolocation/profile_menu_linksownpage.php
@@ -1,16 +1,23 @@
 <script src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=<?= $GLOBALS['google_api'] ?>" type="text/javascript"></script>
 <p>
-	<a href="javascript:show_map_and_marker('map_current', new GLatLng('<?php echo $vars['entity']->current_latitude; ?>', '<?php echo $vars['entity']->current_longitude; ?>'));"><?php echo elgg_echo("geolocation:current_location"); ?></a>
-	<div id="map_current"></div>
+	<a href="javascript:show_map_and_marker('map', new GLatLng('<?php echo $vars['entity']->current_latitude; ?>', '<?php echo $vars['entity']->current_longitude; ?>'));"><?php echo elgg_echo("geolocation:current_location"); ?></a>
 </p>
 <p>
-	<a href="javascript:show_map_and_marker('map_home', new GLatLng('<?php echo $vars['entity']->home_latitude; ?>', '<?php echo $vars['entity']->home_longitude; ?>'));"><?php echo elgg_echo("geolocation:home_location"); ?></a>
-	<div id="map_home"></div>
+	<a href="javascript:show_map_and_marker('map', new GLatLng('<?php echo $vars['entity']->home_latitude; ?>', '<?php echo $vars['entity']->home_longitude; ?>'));"><?php echo elgg_echo("geolocation:home_location"); ?></a>
 </p>
+<div id="layout_map" class="map">
+	<div id="content_area_user_title"><h2>Locations on map</h2></div>
+	<div id="map" style="left:30px;">
+		<div style="padding: 1em; color: gray">Loading...</div>
+	</div>
+</div>
 <script type="text/javascript">
 	function show_map_and_marker(map_id, latlng) {
 		map = new google.maps.Map2(document.getElementById(map_id));
+		map.clearOverlays();
 		map.setCenter(latlng, 13);
 		map.addOverlay(new GMarker(latlng));
+		$('#layout_map').show();
+		$.facebox($('#layout_map'));		
 	}
 </script>

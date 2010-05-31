3dd4476922c21cd095294b341838b53902201ef5
Leonid Dinershtein <leonid.dinershtein@flatsoft.com>
2010-05-31 12:35:11 +0400

    fix bug Setting current location wipes out profile details

--- public/mod/geolocation/views/default/geolocation/edit_location.php
+++ public/mod/geolocation/views/default/geolocation/edit_location.php
@@ -15,6 +15,33 @@
 				$lng = 0;
 	}
 
+        
+
+function get_user_profile_values($vars) {    
+    $html=''; 
+    foreach($vars['config']->profile as $shortname => $valtype) {
+            if ($metadata = get_metadata_byname($vars['entity']->guid, $shortname)) {
+                    if (is_array($metadata)) {
+                            $value = '';
+                            foreach($metadata as $md) {
+                                    if (!empty($value)) $value .= ', ';
+                                    $value .= $md->value;
+                                    $access_id = $md->access_id;
+                            }
+                    } else {
+                            $value = $metadata->value;
+                            $access_id = $metadata->access_id;
+                    }
+            } else {
+                    $value = '';
+                    $access_id = ACCESS_DEFAULT;
+            }
+            $html.= '<input type="hidden" value="'.$value.'" name="'.$shortname.'" />'.chr(13);
+    }
+  return $html;
+}
+
+
 ?>
 <div style="position:relative;width:650px;margin:0 10px;">
 <div class="geosearch single">
@@ -28,7 +55,10 @@
 	<div style="padding: 1em; color: gray">Loading...</div>
 </div></div>
 <form action="<?php echo $vars['url']; ?>action/profile/edit" method="post" id="location_form" style="margin:0 10px;">
-		<?php echo elgg_view('input/securitytoken') ; ?>	
+		<?php echo elgg_view('input/securitytoken') ; ?>
+
+        <?php echo get_user_profile_values($vars); ?>
+
 	<input type="hidden" value="<?php echo $lat; ?>" name="<?php echo $vars['page']; ?>_latitude" id="<?php echo $vars['page']; ?>_geolocation_latitude" />
 	<input type="hidden" value="<?php echo $lng; ?>" name="<?php echo $vars['page']; ?>_longitude" id="<?php echo $vars['page']; ?>_geolocation_longitude" />
 	<?php if ($vars['page'] == 'current'): ?>
@@ -50,7 +80,6 @@
 	}
 
 	function showResponse(response, reverse) {
-
 		if (! response) {
 			alert("Geocoder request failed");
 		} else {

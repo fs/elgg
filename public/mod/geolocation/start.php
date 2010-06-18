4c883160c6344999983ac77fcb47302c53c26e1f
Leonid Dinershtein <leonid.dinershtein@flatsoft.com>
2010-06-18 15:38:15 +0400

    added class object in markup for design

--- public/mod/geolocation/start.php
+++ public/mod/geolocation/start.php
@@ -287,10 +287,14 @@ function geolocation_page_handler($page) {
 					$key = count($data['marker'])-1;
 					$data['marker'][$key]['longitude'] = $item->getLongitude();
 					if (get_subtype_from_id($item->subtype) == 'image') {
-						$data['marker'][$key]['desc'] = '<a class="image-on-map" href="' . $item->getURL() . '">image</a>';
+                                                $data['marker'][$key]['desc'] = '<div class="'.get_subtype_from_id($item->subtype).'">';
+                                                $data['marker'][$key]['desc'] = '<a class="image-on-map" href="' . $item->getURL() . '">image</a>';
+                                                $data['marker'][$key]['desc'] .= '</div>';
 					} else {
-						$data['marker'][$key]['desc'] = '<a class="title" href="' . $item->getURL() . '">' . $item->title . '</a> ';
+                                                $data['marker'][$key]['desc'] = '<div class="'.get_subtype_from_id($item->subtype).'">';
+						$data['marker'][$key]['desc'] .= '<a class="title" href="' . $item->getURL() . '">' . $item->title . '</a> ';
 						$data['marker'][$key]['desc'] .= '<p class="description">' . $item->description . '</p>';
+                                                $data['marker'][$key]['desc'] .= '</div>';
 					}
 
 					if( $item->type == 'object' && in_array(get_subtype_from_id($item->subtype), $icons) ) $data['marker'][$key]['icon'] = get_subtype_from_id($item->subtype);
